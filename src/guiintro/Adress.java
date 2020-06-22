5923d1512bcbe481eca40597375a70308ba35683
basti <basti@DESKTOP-N04QSD5>
2020-06-23 00:48:09 +0200

    Adress Array List

---
+++ src/guiintro/Adress.java
@@ -0,0 +1,53 @@
+package guiintro;
+
+public class Adress {
+	String Vorname;
+	String Nachname;
+	String Plz;
+	String Ort;
+	String Straﬂe;
+
+	public String getVorname() {
+		return Vorname;
+
+	}
+
+	public void setVorname(String neuer_Vorname) {
+		this.Vorname = neuer_Vorname;
+	}
+
+	public String getNachname() {
+		return Nachname;
+	}
+
+	public void setNachname(String neuer_Nachname) {
+		this.Nachname = neuer_Nachname;
+	}
+
+	public String getOrt() {
+		return Ort;
+	}
+
+	public void setOrt(String neuer_Ort) {
+		this.Ort = neuer_Ort;
+	}
+
+	public String getStraﬂe() {
+		return Straﬂe;
+
+	}
+
+	public void setStraﬂe(String neuer_Straﬂe) {
+		this.Straﬂe = neuer_Straﬂe;
+	}
+	
+	public String getPlz() {
+		return Plz;
+
+	}
+
+	public void setPlz(String neuer_Plz) {
+		this.Plz = neuer_Plz;
+	}
+
+}
--- src/guiintro/AdressProject.java
+++ src/guiintro/AdressProject.java
@@ -6,6 +6,8 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import data.Adress;
+
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
